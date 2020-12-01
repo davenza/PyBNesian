1540ef8bcd78130b279db3050382874efc285b7b
David Atienza <daries100@gmail.com>
2020-12-01 12:18:14 +0100

    Fix precision bug for partial linear correlation test.

--- src/learning/independences/continuous/linearcorrelation.cpp
+++ src/learning/independences/continuous/linearcorrelation.cpp
@@ -2,7 +2,7 @@
 #include <boost/math/distributions/students_t.hpp>
 
 using boost::math::students_t_distribution;
-using boost::math::cdf;
+using boost::math::cdf, boost::math::complement;
 
 
 namespace learning::independences::continuous {
@@ -10,6 +10,6 @@ namespace learning::independences::continuous {
     double cor_pvalue(double cor, int df) {
         double statistic = cor * sqrt(df) / sqrt(1 - cor * cor);
         students_t_distribution tdist(static_cast<double>(df));
-        return 2 * (1-cdf(tdist, fabs(statistic)));
+        return 2 * cdf(complement(tdist, fabs(statistic)));
     }
 }

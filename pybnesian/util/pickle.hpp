80ebbc8e4f3a55d162a35b0c3ba07a3c5abe2223
David Atienza <daries100@gmail.com>
2022-01-10 18:34:38 +0100

    Adds pybind11/stl.h header to serialize vector objects.

--- pybnesian/util/pickle.hpp
+++ pybnesian/util/pickle.hpp
@@ -2,19 +2,20 @@
 #define PYBNESIAN_UTIL_PICKLE_HPP
 
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 namespace py = pybind11;
 
 namespace util {
 
 template <typename OBJ>
-void save_object(const OBJ& factor, std::string name) {
+void save_object(const OBJ& obj, std::string name) {
     auto open = py::module_::import("io").attr("open");
 
     if (name.size() < 7 || name.substr(name.size() - 7) != ".pickle") name += ".pickle";
 
     auto file = open(name, "wb");
-    py::module_::import("pickle").attr("dump")(py::cast(&factor), file, 2);
+    py::module_::import("pickle").attr("dump")(py::cast(&obj), file, 2);
     file.attr("close")();
 }
 
